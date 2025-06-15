a83047c2d66ff640110cadebb1f34e6c903efc8b
Roman Garanin <git@romangaranin.net>
2025-06-15 18:44:55 +0200

    feat: add extra tools to mcp

--- mcp_server.py
+++ mcp_server.py
@@ -28,6 +28,31 @@ def read_personal_index_note():
 
 
 @mcp.tool()
+def read_permanent_agent_memory():
+    """
+    Returns permanent memory of the agent, which can be shared between runs.
+    Use it whenever you see something important in the permanent memory, that can be handy for you in the next runs.
+    To be used in combination with write_permanent_memory. Use as often as you see fit.
+    ALWAYS READ IT IN THE BEGINNING OF YOUR RUNS.
+    :return: permanent memory as string
+    """
+    with open("permanent_memory.txt", "r") as file:
+        return file.read()
+
+
+@mcp.tool()
+def write_permanent_agent_memory(text: str):
+    """
+    Writes permanent memory for the LLM/AI agent, which can be shared between runs.
+    To be used in combination with read_permanent_memory. Use as often as you see fit.
+    :param text: text to write, OVERRIDING original data.
+    :return: None
+    """
+    with open("permanent_memory.txt", "w") as file:
+        file.write(text)
+
+
+@mcp.tool()
 def get_notes_by_level(level: int = 1) -> str:
     """
     Returns subset of notes, limited by top notes by given level. Level generally doesn't exceed 4-5.
